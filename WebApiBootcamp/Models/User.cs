9a5e52c79dce7f06e7629d0574fcd0610512623f
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2023-07-11 07:39:37 -0300

    se agrega la carpeta de WebApiBootcamp al proyecto

---
+++ WebApiBootcamp/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApiBootcamp.Models
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+
+        public User() { }
+        public User(int IdUser,string UserName,string Password) 
+        {
+            this.IdUser = IdUser;
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+    }
+}
