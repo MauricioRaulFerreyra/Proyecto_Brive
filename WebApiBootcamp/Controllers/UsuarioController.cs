01191e94104ddef44f5ab4fc421f47919b3b6b6a
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2023-07-11 14:08:45 -0300

    creación de endpoints

---
+++ WebApiBootcamp/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiBootcamp.Controllers
+{
+    [ApiController]
+    [Route("usuario")]
+
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+
+        public dynamic ListarUsuarios()
+        {
+            List<Usuario> usuario = new List<Usuario> 
+            {
+                new Usuario{IdUsuario=1,Correo="mauri",Clave="123"}
+            };
+
+            return usuario;
+        }
+
+        [HttpPost]
+        [Route("guardar")]
+
+        public dynamic GuardarUsuarios(Usuario usuario)
+        {
+            usuario.IdUsuario = 2;
+
+            return new
+            {
+                success = true,
+                message = "Usuario registrado",
+                result = usuario
+            };
+
+        }
+    }
+}
---
+++ WebApiBootcamp/Usuario.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApiBootcamp
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+
+        public Usuario() { }
+        public Usuario(int idUsuario, string correo,string clave)
+        {
+            this.IdUsuario = idUsuario;
+            this.Correo = correo;
+            this.Clave = clave;
+        }
+    }
+}
