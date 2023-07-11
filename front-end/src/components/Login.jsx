b6f65bf0ad1c26a69ea7d867fc308f005c42d4b1
ChemaJob <chema.job.24@gmail.com>
2023-07-11 15:17:58 -0600

    Se agregó la validacion de correo electrónico

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -2,54 +2,54 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function Login({ setIsLoggedIn }) {
-  const [nombre, setNombre] = useState("");
-  const [contraseña, setContraseña] = useState("");
-  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const validUser = "usuario";
+  const validEmail = "usuario@example.com";
   const validPassword = "contraseña";
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (nombre === "" || contraseña === "") {
-      setError(true);
+    if (email === "" || password === "") {
+      setError("Todos los campos son obligatorios");
       return;
     }
-    if (nombre !== validUser || contraseña !== validPassword) {
-      setError(true);
-      alert("Usuario o contraseña incorrectos");
-    } else {
-      setError(false);
+
+    if (email === validEmail && password === validPassword) {
+      setError("");
       setIsLoggedIn(true); // Establecer isLoggedIn en verdadero
       navigate("/home");
+    } else if (email !== validEmail) {
+      setError("La cuenta no existe");
+    } else {
+      setError("Contraseña incorrecta");
     }
   }
 
   return (
     <div className="login-container">
       <section className="Login">
-        <h1 className="login-title">Inicia Sesion</h1>
+        <h1 className="login-title">Inicia Sesión</h1>
         <form className="formulario" onSubmit={handleSubmit}>
           <input
             className="login-input"
-            type="text"
-            placeholder="Usuario"
-            value={nombre}
-            onChange={(e) => setNombre(e.target.value)}
+            type="email"
+            placeholder="Correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="login-input"
             type="password"
             placeholder="Contraseña"
-            value={contraseña}
-            onChange={(e) => setContraseña(e.target.value)}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button className="login-button">Iniciar Sesión</button>
         </form>
-        {error && !!(nombre === "" || contraseña === "") && (
-          <p className="error-message">Todos los campos son obligatorios</p>
-        )}
+        {error && <p className="error-message">{error}</p>}
       </section>
     </div>
   );
