fbac83c389517e869fd1dbba83238224a3882e14
ChemaJob <chema.job.24@gmail.com>
2023-07-11 16:53:53 -0600

    Se separo logica de vista

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,32 +1,15 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLoginForm } from "../hooks/useLoginForm";
 
 export function Login({ setIsLoggedIn }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
 
-  const validEmail = "usuario@example.com";
-  const validPassword = "contraseña";
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (email === "" || password === "") {
-      setError("Todos los campos son obligatorios");
-      return;
-    }
-
-    if (email === validEmail && password === validPassword) {
-      setError("");
-      setIsLoggedIn(true); // Establecer isLoggedIn en verdadero
-      navigate("/home");
-    } else if (email !== validEmail) {
-      setError("La cuenta no existe");
-    } else {
-      setError("Contraseña incorrecta");
-    }
-  }
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    error,
+    handleSubmit,
+  } = useLoginForm(setIsLoggedIn);
 
   return (
     <div className="login-container">
