d7ac5d41ef33087bb3c4629d71da88d3b57369ee
YessVizcarra <yessvizcarra@YessVizcarras-MacBook-Pro.local>
2023-07-12 17:28:34 -0500

    modifique lo de cors en la api y cree el request para el formulario

--- front-end/src/hooks/useLoginForm.js
+++ front-end/src/hooks/useLoginForm.js
@@ -7,9 +7,6 @@ export function useLoginForm(setIsLoggedIn) {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const validEmail = "usuario@example.com";//email
-  const validPassword = "contraseña";//password
-
   function handleSubmit(e) {
     e.preventDefault();
     if (email === "" || password === "") {
@@ -17,15 +14,21 @@ export function useLoginForm(setIsLoggedIn) {
       return;
     }
 
-    if (email === validEmail && password === validPassword) {
-      setError("");
-      setIsLoggedIn(true); // Establecer isLoggedIn en verdadero
-      navigate("/home");
-    } else if (email !== validEmail) {
-      setError("La cuenta no existe");
-    } else {
-      setError("Contraseña incorrecta");
-    }
+    fetch('https://localhost:7146/usuario/listar').then(res => res.json())
+        .then(data => {
+          const userExist = data.filter(user => user.correo === email && user.clave === password);
+
+          if(userExist.length > 0){
+            setError("");
+            setIsLoggedIn(true); // Establecer isLoggedIn en verdadero
+            navigate("/home");
+          }
+          else {
+            setError("EL correo o la contraseña son incorrectos");
+          }
+          
+        })
+        .catch(err => console.log(err));
 
     // fetch('https://jsonplaceholder.typicode.com/posts', {
     //   method: 'POST',
