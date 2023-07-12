4e0e60350cab4b7114337cbce259f5df6f6d1c81
ChemaJob <chema.job.24@gmail.com>
2023-07-12 17:50:09 -0600

    se corrige useLoginForm

--- front-end/src/hooks/useLoginForm.js
+++ front-end/src/hooks/useLoginForm.js
@@ -1,60 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-export function useLoginForm(setIsLoggedIn) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
-
-  const validEmail = "usuario@example.com";//email
-  const validPassword = "contraseña";//password
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (email === "" || password === "") {
-      setError("Todos los campos son obligatorios");
-      return;
-    }
-
-    if (email === validEmail && password === validPassword) {
-      setError("");
-      setIsLoggedIn(true); // Establecer isLoggedIn en verdadero
-      navigate("/home");
-    } else if (email !== validEmail) {
-      setError("La cuenta no existe");
-    } else {
-      setError("Contraseña incorrecta");
-    }
-
-    // fetch('https://jsonplaceholder.typicode.com/posts', {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     email: 'foo',
-    //     body: 'bar',
-    //     userId: 1,
-    //   }),
-    //   headers: {
-    //     'Content-type': 'application/json; charset=UTF-8',
-    //   },
-    // })
-    //   .then((response) => response.json())
-    //   .then((json) => console.log(json));
-  }
-  
-  return {
-    email,
-    setEmail,
-    password,
-    setPassword,
-    error,
-    handleSubmit,
-  };
-}
-
-/* Descomentar cuando funcione el enlace
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export function useLoginForm(setIsLoggedIn) {
@@ -74,18 +19,15 @@ export function useLoginForm(setIsLoggedIn) {
       const response = await axios.post(
         "https://localhost:7146/prueba/guardar/api/usuario/validar",
         {
-          method: 'POST',
-          body: JSON.stringify({
-            correo: email,
-            clave: password,
-          }),
+          correo: email,
+          clave: password
+        },
+        {
           headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
+            'Content-Type': 'application/json'
+          }
         }
-      )
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+      );
 
       const { success, message } = response.data;
 
@@ -111,4 +53,3 @@ export function useLoginForm(setIsLoggedIn) {
     handleSubmit,
   };
 }
-*/
